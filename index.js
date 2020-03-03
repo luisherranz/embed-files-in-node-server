4ad4897e4a7d47586d1ef0ae51548a51356c3a04
Luis Herranz <luisherranz@gmail.com>
2020-03-03 11:53:50 +0100

    Embed plain files

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const Koa = require("koa");
+const { get } = require("koa-route");
 
 const app = new Koa();
 
+app.use(
+  get("/static/:filename", (ctx, filename) => {
+    // Set response body.
+    ctx.body = require(`!!raw-loader!./build/static/${filename}`).default;
+    // Change content type for javascript files.
+    if (filename.endsWith(".js")) ctx.type = "application/javascript";
+  })
+);
+
 app.use(ctx => {
-  ctx.body = "Hi!";
+  ctx.body = "Server is working.";
 });
 
 module.exports = app.callback();
